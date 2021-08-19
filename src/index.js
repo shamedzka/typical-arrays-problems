54da029cb7b5ffc4d36d80ade624c06463e51c49
shamedzka <shamedzka@gmail.com>
2021-08-19 22:53:13 +0300

    add avg and zero

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  else {
+
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  else {
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  else {
+    let i = 0;
+    let sum = 0;
+    while (i < array.length) {
+      sum = sum + array[i];
+      i = i + 1;
+    }
+    return (sum / array.length);
+  }
 }
